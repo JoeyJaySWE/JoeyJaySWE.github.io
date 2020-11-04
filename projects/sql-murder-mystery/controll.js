90b90b696b9a370f6c38e5a8dae45699847738f5
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2020-11-04 14:45:45 +0100

    broke out the controller to its own function

--- projects/sql-murder-mystery/controll.js
+++ projects/sql-murder-mystery/controll.js
@@ -3,11 +3,14 @@ let murder_name = document.getElementsByName("name");
 let murder_weapon = document.getElementsByName("weapon");
 let murder_location = document.getElementsByName("where");
 
-arrest.addEventListner("click", function(){
+arrest.addEventListner("click", find_murder);
+
+
+function find_murder(){
     if(murder_name === "Jack Skellington" && murder_weapon === "Poison" && murder_location === "NK"){
         window.location.href = "correct.html";
     }
     else{
         window.location.href = "wrong.html";
     }
-});
+};

84d65fdb82391981ba269bf32cca908d4a172e5b
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2020-11-04 15:21:12 +0100

    Final commit

--- projects/sql-murder-mystery/controll.js
+++ projects/sql-murder-mystery/controll.js
@@ -1,16 +1,20 @@
-let arrest = document.getElementById("arrest");
-let murder_name = document.getElementsByName("name");
-let murder_weapon = document.getElementsByName("weapon");
-let murder_location = document.getElementsByName("where");
-
-getElementById("arrest").addEventListener("click", find_murder);
-
-
 function find_murder(){
+
+let murder_name = document.getElementById("name").value;
+let murder_weapon = document.getElementById("weapon").value;
+let murder_location = document.getElementById("where").value;
+
+
+
     if(murder_name === "Jack Skellington" && murder_weapon === "Poison" && murder_location === "NK"){
         window.location.href = "correct.html";
+        console.log("Right!");
     }
     else{
-        window.location.href = "wrong.html";
+        window.location.href = "wrong.html?name="+murder_name+"&weapon="+murder_weapon+"&location="+murder_location;
+        // console.log("Wrong");
+        // console.log(murder_name);
+        // console.log(murder_weapon);
+        // console.log(murder_location);
     }
 };
